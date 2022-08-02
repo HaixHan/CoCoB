5d3edf1846e3363671510895e54cb9b69bfcbc44
Haixia <68429287+HaixHan@users.noreply.github.com>
2022-08-02 19:15:33 +0800

    Update KMaxValue.py

--- KMaxValue.py
+++ KMaxValue.py
@@ -1,9 +1,9 @@
 class kMaxValues:
     def kMaxValues(self, List, k):
-        Lst = List[:]  # 对列表进行浅复制，避免后面更改原列表数据
+        Lst = List[:]  
         index_k = []
         for i in range(k):
-            index_i = Lst.index(max(Lst))  # 得到列表的最d大值，并得到该最小值的索引
-            index_k.append(index_i)  # 记录最小值索引
-            Lst[index_i] = float('-inf')  # 将遍历过的列表最小值改为负无穷大，下次不再选择
+            index_i = Lst.index(max(Lst))  
+            index_k.append(index_i) 
+            Lst[index_i] = float('-inf')  
         return index_k
